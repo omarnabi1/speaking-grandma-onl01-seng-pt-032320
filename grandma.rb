35958c3aa871faa078083dfa58b594dd49d352ca
Omar Nabi <onabi20@gmail.com>
2020-03-26 03:58:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,7 @@
 def speak_to_grandma(string)
+  
+end
+
   if string == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif string != string.upcase
@@ -6,7 +9,6 @@ def speak_to_grandma(string)
   else string.upcase
     puts "NO, NOT SINCE 1938!"
   end
-end
 
 
 
