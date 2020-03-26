80fa39e707c13c20dcf64c02745fb812ae4b7e5d
Omar Nabi <onabi20@gmail.com>
2020-03-26 04:00:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(string)
   
-  if string == "I LOVE YOU GRANDMA!"
+  if string == 'I LOVE YOU GRANDMA!'
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif string != string.upcase
     puts "HUH?! SPEAK UP, SONNY!"
